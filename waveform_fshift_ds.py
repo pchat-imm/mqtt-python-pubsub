2f1ac05609d292593daaf97fa16fb2728c2282dd
pchat-imm <pchat.imm@gmail.com>
2025-02-05 16:03:30 +0700

    add comment and explanation

--- waveform_fshift_ds.py
+++ waveform_fshift_ds.py
@@ -1,23 +1,13 @@
-## import requirement
 import numpy as np
 import scipy
 import csv
-# import matplotlib.pyplot as plt
-# !python3 -m pip install py3gpp
-# from py3gpp import *
-# import sys
 import os
-
 import paho.mqtt.client as mqtt
-
-
-
 
 # Set up client and callbacks
 client = mqtt.Client()
-# global count_work 
+# global parameter to count number of work
 count_work = 0
-
 
 # Define callback functions
 def on_connect(client, userdata, flags, rc):
@@ -27,32 +17,40 @@ def on_connect(client, userdata, flags, rc):
 # This is called when a message is received on a subscribed topic
 def on_message(client, userdata, msg):
     print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
     if(msg.topic =="waveform_fshift_ds/start"):
-      sampleRate = 15.36e6
+      # if work_end : do_nothing
       if os.environ['fshift'] == "end":
-         print(".")
+        pass
       
+      # other work start to calculate frequency_shift and downsampling
       else:
-        fshift = float(os.environ['fshift'])
-        print("load file")
+        sampleRate = 15.36e6
+        fshift = float(os.environ['fshift'])      ## receive input from when running the script
+        
+        print("load")
         waveform = load_rx_srsRAN('./waveform_IQComplex_fllay.csv')
-        print("freq_shift")
+        
+        print("calculate")
         rxWaveformFreqCorrected = waveform_fshift(fshift, sampleRate, waveform)
         rxWaveformDS = waveformDS(sampleRate, rxWaveformFreqCorrected)
+
         ## add function correlation
+        refWaveforms = load_PSS_seq()
         print("run")
 
         client.publish("waveform_fshift_ds/work"+os.environ['fshift'], payload=fshift, qos=0, retain=False)
 
     else:
-       global count_work
+       global count_work    # call to edit global parameter
        count_work = count_work + 1
        count_complete(count_work)
-       # return count_work
+
     
 def count_complete(count_work):
     if(count_work == 3):
-      # compare correlate
+      # count_work here is local parameter
+      ## start compare correlate
       print("sum")
 
 
@@ -71,10 +69,7 @@ client.username_pw_set("user_staff6", "gv76seyLDNtP6Zgz3!WYYc!fK")
 client.connect("mqtt.lailab.online", 10001, 60)
 
 if(os.environ['fshift'] == "end"):
-  # client.subscribe("waveform_fshift_ds/work0")
-  # client.subscribe("waveform_fshift_ds/work1")
-  # client.subscribe("waveform_fshift_ds/work2")
-  # client.subscribe("waveform_fshift_ds/work3")
+  # work_end have to subscribe all work, to calculate count_work
   client.subscribe("waveform_fshift_ds/#")
 else:
    client.subscribe("waveform_fshift_ds/start")
@@ -104,6 +99,34 @@ def waveformDS(sampleRate, waveform):
   syncSR = syncNfft * scs * 1e3
   rxWaveformDS = scipy.signal.resample_poly(waveform, syncSR, sampleRate)
   return rxWaveformDS
+
+
+def load_PSS_seq():
+  # base path
+  base_path = '/home/chatchamon/workarea/npn_5g/bladeRF_txrx/based_IQ_find_NID2/trial/PSS_seq'
+  NID2_val = [0,1,2]
+
+  # Dictionary to store file path and refWaveform
+  PSS_files = {}
+  refWaveforms = {}
+
+  # Generate file path and initialize refWaveform arrays
+  # add lists in the dictionary
+  for NID2 in NID2_val:
+      PSS_files[f'NID2_{NID2}'] = f'{base_path}/NID2_{NID2}.csv'
+      refWaveforms[f'NID2_{NID2}'] = []
+
+  for NID2 in NID2_val:
+      with open(PSS_files[f'NID2_{NID2}'], 'r') as f:
+          reader = csv.reader(f)
+          for row in reader:
+              IQ_PSS = row[0].strip()     # get string
+              IQ_PSS = complex(IQ_PSS)    # turn to complex
+              refWaveforms[f'NID2_{NID2}'].append(IQ_PSS)
+  return refWaveforms
+
+
+
 
 if __name__ == "__main__":
   # sampleRate = 15.36e6
